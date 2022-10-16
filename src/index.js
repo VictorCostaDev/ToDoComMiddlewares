824cdc731d13b7a28f06f7b9b4d6b63d17064ed2
Victor Gabriel <victo091198@gmail.com>
2022-10-16 19:19:37 -0300

    Updated in Middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,52 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) return response.status(404).json({error : 'User not exists'});
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const isNotAvailableCreateTodo = user.pro === false && user.todos.length >= 10;
+
+  if(isNotAvailableCreateTodo) return response.status(403);
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;  
+
+  const user = users.find(user => user.username === username); 
+  if(!user) return response.status(404).json({error : 'User not exists'});
+
+  const idValid = validate(id);
+  if (!idValid) return response.status(400);
+
+  const todo = user.todos.find( todo => todo.id === id);
+  if (!todo) return response.status(404);
+
+  request.todo = todo;
+  request.user = user;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find( user => user.id === id);
+  if(!user) return response.status(404).json({error : 'User not exists'});
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -30,10 +63,8 @@ app.post('/users', (request, response) => {
 
   const usernameAlreadyExists = users.some((user) => user.username === username);
 
-  if (usernameAlreadyExists) {
-    return response.status(400).json({ error: 'Username already exists' });
-  }
-
+  if (usernameAlreadyExists) return response.status(400).json({ error: 'Username already exists' });
+  
   const user = {
     id: uuidv4(),
     name,
@@ -117,7 +148,7 @@ app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, re
 
   user.todos.splice(todoIndex, 1);
 
-  return response.status(204).send();
+  return response.status(204).json();
 });
 
 module.exports = {
